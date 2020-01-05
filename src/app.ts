e9e6ea697d1106e84e7da4ada60c8cca35394290
4doge <antonboksha@gmail.com>
2020-01-05 22:20:04 +0200

    init basic phaser project structure

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import 'phaser';
+import { MainScene } from './scenes/main';
+
+const config: Phaser.Types.Core.GameConfig = {
+  width: 800,
+  height: 600,
+  type: Phaser.AUTO,
+  parent: 'game',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {
+        y: 200,
+      },
+    },
+  },
+  scene: MainScene,
+};
+
+export class Game extends Phaser.Game {
+  constructor(config: Phaser.Types.Core.GameConfig) {
+    super(config);
+  }
+}
+
+window.addEventListener('load', () => {
+  const game = new Game(config);
+});
